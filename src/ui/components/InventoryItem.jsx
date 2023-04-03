0a6c324bfc87ccad554ff56d520c1ed7cf321bfc
julia <julia.albq@gmail.com>
2023-04-03 15:07:33 -0300

    refactor: implement changes related to the functions calculateWear, representByWearTerm and representByFragilityTerm modifications, add the constant className and add classes to tags h2 and p

--- src/ui/components/InventoryItem.jsx
+++ src/ui/components/InventoryItem.jsx
@@ -1,21 +1,24 @@
-import { representFragilityByTerm } from "../../domain/representFragilityByTerm";
-import { representWearByTerm } from "../../domain/representWearByTerm";
+import { representFragilityByTerm } from "../../utils/representFragilityByTerm";
+import { representWearByTerm } from "../../infrastructure/inner/representWearByTerm";
 import cx from "./InventoryItem.module.scss";
+import { calculateWear } from "../../domain/calculateWear";
+import { itemWearClassTable } from "../../utils/itemWearClassTable";
 
 export const InventoryItem = ({ item }) => {
   const itemFragility = representFragilityByTerm(item.fragility);
-  const itemWear = representWearByTerm(item.wear);
-  const iWear = itemWear.toLowerCase().replace(" h", "H");
+  const itemWear = calculateWear(item.fragility, item.lastMaintenance)
+  const representedItemWear = representWearByTerm(itemWear);
+  const className = itemWearClassTable[representedItemWear]
 
   return (
-    <div className={`${cx.itemContainer} ${cx[iWear]}`}>
-      <h2>
+    <div className={`${cx.itemContainer} ${cx[className]}`}>
+      <h2 className={cx.itemTitle}>
         {item.type} | {item.brand} {item.model}
       </h2>
-      <p>Sector: {item.sector}</p>
-      <p>Fragility: {itemFragility}</p>
-      <p>Last Maintenance: {item.lastMaintenance}</p>
-      <p>Wear: {itemWear}</p>
+      <p className={cx.itemInfo}>Sector: {item.sector}</p>
+      <p className={cx.itemInfo}>Fragility: {itemFragility}</p>
+      <p className={cx.itemInfo}>Last Maintenance: {item.lastMaintenance.day}/{item.lastMaintenance.month}/{item.lastMaintenance.year}</p>
+      <p className={cx.itemInfo}>Wear: {representedItemWear}</p>
     </div>
   );
 };
